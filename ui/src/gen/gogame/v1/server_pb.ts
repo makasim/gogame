8ff576bab187bf5ea0d9b2ae733ca211c7ffb514
Misha Grinko <glukym@gmail.com>
2024-08-17 13:27:12 +0300

    [UI] set default game duration

--- ui/src/gen/gogame/v1/server_pb.ts
+++ ui/src/gen/gogame/v1/server_pb.ts
@@ -403,6 +403,11 @@ export class CreateGameRequest extends Message<CreateGameRequest> {
    */
   player1?: Player;
 
+  /**
+   * @generated from field: int32 move_duration_sec = 3;
+   */
+  moveDurationSec = 0;
+
   constructor(data?: PartialMessage<CreateGameRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -413,6 +418,7 @@ export class CreateGameRequest extends Message<CreateGameRequest> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "name", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "player1", kind: "message", T: Player },
+    { no: 3, name: "move_duration_sec", kind: "scalar", T: 5 /* ScalarType.INT32 */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): CreateGameRequest {
